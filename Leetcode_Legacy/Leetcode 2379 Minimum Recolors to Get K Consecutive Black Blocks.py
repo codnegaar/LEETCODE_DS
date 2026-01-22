cbedfe09cbc6da4eeb41e57a0f2ba360c1a974ea
Reza <seda1390@gmail.com>
2026-01-22 08:07:01 -0800

    Update and rename 2379 Minimum Recolors to Get K Consecutive Black Blocks.py to Leetcode 2379 Minimum Recolors to Get K Consecutive Black Blocks.py

--- Leetcode_Legacy/Leetcode 2379 Minimum Recolors to Get K Consecutive Black Blocks.py
+++ Leetcode_Legacy/Leetcode 2379 Minimum Recolors to Get K Consecutive Black Blocks.py
@@ -1,5 +1,5 @@
 ﻿'''
-2379 Minimum Recolors to Get K Consecutive Black Blocks
+Leetcode 2379 Minimum Recolors to Get K Consecutive Black Blocks
  
 You are given a 0-indexed string blocks of length n, where blocks[i] is either 'W' or 'B', representing the color of the ith block. 
 The characters 'W' and 'B' denote the colors white and black, respectively.
