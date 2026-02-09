f9d920a41fbc6f013cd9007b69c913d8a531f088
Reza <seda1390@gmail.com>
2026-02-08 18:37:56 -0800

    Add problem description for balancing BST
    Added problem statement for Leetcode 1382.

--- Leetcode_Legacy/Leetcode 1382 Balance a Binary Search Tree.py
+++ Leetcode_Legacy/Leetcode 1382 Balance a Binary Search Tree.py
@@ -1,5 +1,4 @@
 ﻿'''
-
 Leetcode 1382 balance a Binary Search Tree
 
 Given the root of a binary search tree, return a balanced binary search tree with the same node values. If there is more than one answer, return any of them.
